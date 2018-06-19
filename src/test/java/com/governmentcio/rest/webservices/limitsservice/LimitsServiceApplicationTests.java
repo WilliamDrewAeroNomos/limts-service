8026f3f54ab34f3e5925fc6accabb4d66feac288
William Drew <wdrew@governmentcio.com>
2018-06-19 10:59:50 -0400

    Final changes for repackaging

--- src/test/java/com/governmentcio/rest/webservices/limitsservice/LimitsServiceApplicationTests.java
+++ src/test/java/com/governmentcio/rest/webservices/limitsservice/LimitsServiceApplicationTests.java
@@ -5,6 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * 
+ * @author <a href=mailto:support@governmentcio.com>support</a>
+ * 
+ *         Please document me!!
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LimitsServiceApplicationTests {
